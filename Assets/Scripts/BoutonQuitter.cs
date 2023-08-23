1c0e39ad265719bdc166b8f2aff2d9926e7ad360
Alexandre Ouellet <ouellet.alexandre@cegepvicto.ca>
2023-08-23 17:48:38 -0400

    Quitter

---
+++ Assets/Scripts/BoutonQuitter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoutonQuitter : MonoBehaviour
+{
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
